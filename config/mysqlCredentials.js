530bb15aa94825ae97a990ea1a0fe2ceac464c70
Aneja Orlic <aneja.orlic@fer.hr>
2020-06-07 17:46:05 +0200

    mysql

--- config/mysqlCredentials.js
+++ config/mysqlCredentials.js
@@ -1,7 +1,7 @@
 module.exports = {
     database: 'orderio',
     user:     'root',
-    password: 'root', //'Orderioadmin2020!',
+    password: 'Orderioadmin2020!', //'Orderioadmin2020!',
     sessionSecret: 'express session secret'
 }
 
